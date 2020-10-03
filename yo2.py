85cb444590d6bf16792279ccd366c957683f8ed1
mitraj405 <47468515+mitraj405@users.noreply.github.com>
2020-10-03 22:59:17 +0530

    Update yo2.py

--- yo2.py
+++ yo2.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 url="https://dhruvjasani.codes/"
 result=requests.get(url)
-r=result.content
-s=BeautifulSoup(r,'html.parser')
+r1=result.content
+s=BeautifulSoup(r1,'html.parser')
 
 print(s.prettify())
